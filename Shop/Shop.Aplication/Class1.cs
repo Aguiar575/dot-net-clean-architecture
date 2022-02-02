cad5eea7ce3a5deac09a38f56eba8ac643aa6113
Aguiar575 <aguiarus99arthurus@gmail.com>
2022-02-02 00:00:32 -0300

    initial commit of project; People entitie added on Shop.Domain;

---
+++ Shop/Shop.Aplication/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Shop.Aplication
+{
+    public class Class1
+    {
+    }
+}
---
+++ Shop/Shop.Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Shop.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
